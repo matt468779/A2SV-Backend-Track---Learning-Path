6b40734be7f837f5e586253615a7f77472554353
matt468779 <mengistusima@gmail.com>
2023-09-07 21:36:21 +0300

    Done task 2

--- Fundamentals of C#/Program.cs
+++ Fundamentals of C#/Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Task;
 
-Task1 task1 = new Task1();
-task1.DoTask();
+// Task 1
+// Task1 task1 = new Task1();
+// task1.DoTask();`
+
+// Task 2
+Task2 task2 = new Task2();
+string example = "The the THE Quick quick brown fox FoX";
+task2.DoTask(example);
--- Fundamentals of C#/Task2.cs
+++ Fundamentals of C#/Task2.cs
@@ -1,7 +1,43 @@
+using System.Xml.XPath;
+
 namespace Task
 {
     class Task2
     {
+        public Dictionary<string, int> GetFrequencyOfWords(string sentence)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
 
+            foreach (var word in sentence.Split())
+            {
+                string lower_word = word.ToLower();
+                if (result.ContainsKey(lower_word))
+                {
+                    result[lower_word]++;
+                }
+                else
+                {
+                    result[lower_word] = 1;
+                }
+            }
+
+            return result;
+        }
+
+        public void DoTask(string sentence)
+        {
+            Dictionary<string, int> result = GetFrequencyOfWords(sentence);
+            DisplayResult(sentence, result);
+        }
+
+        public void DisplayResult(string sentence, Dictionary<string, int> dict)
+        {
+            Console.WriteLine($"Input word: {sentence}");
+            Console.WriteLine($"The fequency of each word in lowercase is: ");
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} ==> {item.Value}");
+            }
+        }
     }
 }
